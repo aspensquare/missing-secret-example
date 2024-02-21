945d5c9af2bae064e225280bfc407c7294bbb9aa
Christopher Primavera <prim1013@gmail.com>
2024-02-21 07:01:16 -0500

    pw

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -60,7 +60,7 @@ function SignInForm( { callbackUrl } ) {
                         // redirect: true,
                         // redirectTo: callbackUrl,
                         username: "chris_primavera@aspensquare.com",
-                        password: "Prim6006#"
+                        password: "****"
                     } );
                 }}
             >
